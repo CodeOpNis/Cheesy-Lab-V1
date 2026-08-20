ea8b0a44ba413fc1125e70bd2e6e13129f8a669e
CodeOpNis <singhnishantacc@gmail.com>
2026-08-20 11:39:21 +0530

    port configuration

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
